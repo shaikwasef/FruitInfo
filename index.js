2fa12629f639433e6a903a0ee36aef4734ecd3aa
shaikwasef <wasefshaik@gmail.com>
2021-03-02 20:58:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ var submit = document.getElementById("submit");
 submit.addEventListener("click", request_data);
 
 function request_data() {
-   var request = new XMLHttpRequest();
-   var url = "https://www.anapioficeandfire.com/api/characters/?name=" + input.value.replaceAll(" ","%20");
-   request.open('GET',url,true);
-   request.onload = () => {
-     var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400){
-      console.log(data[0].name);
-      console.log(data[0].born);
-      console.log(data[0].gender);
-    };
-}
+  var request = new XMLHttpRequest();
+  var url =
+    "https://www.anapioficeandfire.com/api/characters/?name=" +
+    input_text.value.replaceAll(" ", "%20");
+  console.log(url);
+  request.open("GET", url, true);
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      console.log(data);
+    }
+  };
 }
